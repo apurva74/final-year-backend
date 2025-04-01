1429d46fd8b9d4dd169b687b3445f909cb4e8eab
Apurva Tiwari <139570451+apurva74@users.noreply.github.com>
2025-04-01 12:48:07 +0530

    Delete AuthRoutes.js

--- AuthRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerUser } from "../controllers/authController.js";  // ✅ Import controller
-
-const router = express.Router();
-
-router.post("/register", registerUser);  // ✅ Define registration route
-
-export default router;
